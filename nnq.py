8967f7ab7f30bf94cee29a6edac14c38074200c6
Nunung Nurul Qomariyah <nnqomariyah@gmail.com>
2022-04-27 12:29:38 +0700

    Update nnq.py

--- nnq.py
+++ nnq.py
@@ -8,6 +8,7 @@ clf = pickle.load(open('pac_model.pkl', 'rb'))
 tf1 = pickle.load(open("tfidf1.pkl", 'rb'))
 
 X_test = pd.read_csv('testdata.csv', error_bad_lines=False, encoding='latin1')
+inputdata = input("Enter your text: ")
 
 
 # Create new tfidfVectorizer with old vocabulary
@@ -15,7 +16,7 @@ factory = StopWordRemoverFactory()
 stopwords = factory.get_stop_words()
 tf1_new = TfidfVectorizer(analyzer='word', ngram_range=(1,2), stop_words=frozenset(stopwords), lowercase = True,
                           max_features = 500000, vocabulary = tf1.vocabulary_)
-X_tf1 = tf1_new.fit_transform(X_test.iloc[0])
+X_tf1 = tf1_new.fit_transform([inputdata])
 
 pred = clf.predict(X_tf1)
 print(pred)
